1b1ceb770589de7414371990213e0787d689a78a
jrichterjax <jrichterjax@gmail.com>
2023-05-13 17:52:42 -0400

    update

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,43 +1,66 @@
+-- Drop table if exists
+DROP TABLE departments;
+DROP TABLE titles;
+DROP TABLE employees;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE salaries;
+
+
 -- Create a table of departments
 CREATE TABLE departments (
-  dept_no INTEGER NOT NULL,
-  dept_name VARCHAR NOT NULL,
+  dept_no VARCHAR,
+  dept_name VARCHAR,
   PRIMARY KEY (dept_no)
-);
-
--- Create a table of department_emp
-CREATE TABLE department_emp (
-  emp_no INTEGER NOT NULL,
-  dept_no INTEGER NOT NULL  
-);
-
--- Create a table of dept_manager
-CREATE TABLE dept_manager (
-  dept_no INTEGER NOT NULL,
-  emp_no INTEGER NOT NULL
-);
-
--- Create a table of employees
-CREATE TABLE employees (
-  emp_no INTEGER NOT NULL,
-  emp_title_id INTEGER NOT NULL,
-  birth_date
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL,
-  sex VARCHAR NOT NULL,
-  hire_date
-  PRIMARY KEY (emp_no)
-);
-
--- Create a table of salaries
-CREATE TABLE salaries (
-  emp_no INTEGER NOT NULL,
-  salary INTEGER NOT NULL
 );
 
 -- Create a table of titles
 CREATE TABLE titles (
-  title_id INTEGER NOT NULL,
-  title VARCHAR NOT NULL,
-  PRIMARY KEY (title_no)
+  title_id VARCHAR,
+  title VARCHAR,
+  PRIMARY KEY (title_id)
 );
+
+-- Create a table of employees
+CREATE TABLE employees (
+  emp_no INTEGER,
+  emp_title_id VARCHAR,
+  birth_date DATE,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  sex VARCHAR,
+  hire_date DATE,
+  PRIMARY KEY (emp_no),
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create a table of department_emp
+CREATE TABLE dept_emp (
+  emp_no INTEGER,
+  dept_no VARCHAR,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create a table of dept_manager
+CREATE TABLE dept_manager (
+  dept_no VARCHAR,
+  emp_no INTEGER,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create a table of salaries
+CREATE TABLE salaries (
+  emp_no INTEGER,
+  salary INTEGER,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+select * from departments;
+select * from titles;
+select * from employees;
+select * from dept_emp;
+select * from dept_manager;
+select * from salaries;
