c8daa3c753b24adc419798d9d080bcfbd20ea231
jrichterjax <jrichterjax@gmail.com>
2023-05-16 18:19:32 -0400

    update

--- EmployeeSQL/tables.sql
+++ EmployeeSQL/tables.sql
@@ -8,26 +8,26 @@ DROP TABLE salaries;
 
 -- Create a table of departments
 CREATE TABLE departments (
-  dept_no VARCHAR,
-  dept_name VARCHAR,
+  dept_no VARCHAR(10) NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (dept_no)
 );
 
 -- Create a table of titles
 CREATE TABLE titles (
-  title_id VARCHAR,
-  title VARCHAR,
+  title_id VARCHAR(10) NOT NULL,
+  title VARCHAR(30) NOT NULL,
   PRIMARY KEY (title_id)
 );
 
 -- Create a table of employees
 CREATE TABLE employees (
-  emp_no INTEGER,
-  emp_title_id VARCHAR,
+  emp_no INTEGER NOT NULL,
+  emp_title_id VARCHAR(10) NOT NULL,
   birth_date DATE,
-  first_name VARCHAR,
-  last_name VARCHAR,
-  sex VARCHAR,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  sex VARCHAR(10),
   hire_date DATE,
   PRIMARY KEY (emp_no),
   FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
@@ -35,24 +35,27 @@ CREATE TABLE employees (
 
 -- Create a table of department_emp
 CREATE TABLE dept_emp (
-  emp_no INTEGER,
-  dept_no VARCHAR,
+  emp_no INTEGER NOT NULL,
+  dept_no VARCHAR(10) NOT NULL,
+  PRIMARY KEY (emp_no, dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Create a table of dept_manager
 CREATE TABLE dept_manager (
-  dept_no VARCHAR,
-  emp_no INTEGER,
+  dept_no VARCHAR(10) NOT NULL,
+  emp_no INTEGER NOT NULL,
+  PRIMARY KEY (dept_no, emp_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Create a table of salaries
 CREATE TABLE salaries (
-  emp_no INTEGER,
-  salary INTEGER,
+  emp_no INTEGER NOT NULL,
+  salary INTEGER NOT NULL,
+  PRIMARY KEY (emp_no, salary),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # sql-challenge
+
+Data Modeling - ERD image
+
+
+
+Data Engineering - table schemata and tables.sql file
+
+
+
+
+Data Analysis - the queries included in this repository produce the following: 
+
+- The employee number, last name, first name, sex, and salary for each employee.
+- The first name, last name, and hire date for the employees who were hired in 1986.
+- The manager of each department along with their department number, department name, employee number, last name, and first name.
+- The department number for each employee along with that employee’s employee number, last name, first name, and department name.
+- The first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+- Each employee in the Sales department, including their employee number, last name, and first name.
+- Each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+- The frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+
